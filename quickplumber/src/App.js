8269970c713eee5268e564e545cf0209344e34fd
dimxn <zakermen.d@gmail.com>
2023-09-07 21:52:36 +0300

    first commit

---
+++ quickplumber/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <header className="header">
+      <div className="container">
+        <h1 className="title">QuickPlumber</h1>
+      </div>
+    </header>
+  );
+}
+
+export default App;
